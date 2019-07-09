e524fa446f437b84ad9a48fe469955c1ff49fcbb
Sandra Roman Rivera <sroman98@fb.com>
2019-07-09 11:37:31 -0700

    add user logout

---
+++ SRInstagram/View Controllers/HomeViewController.h
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.h
+//  SRInstagram
+//
+//  Created by sroman98 on 7/9/19.
+//  Copyright © 2019 sroman98. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SRInstagram/View Controllers/HomeViewController.m
@@ -0,0 +1,51 @@
+//
+//  HomeViewController.m
+//  SRInstagram
+//
+//  Created by sroman98 on 7/9/19.
+//  Copyright © 2019 sroman98. All rights reserved.
+//
+
+#import "HomeViewController.h"
+#import "LoginViewController.h"
+#import "Parse/Parse.h"
+#import "AppDelegate.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didTapLogout:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+        if([PFUser currentUser] == nil) {
+            AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+            UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+            LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+            appDelegate.window.rootViewController = loginViewController;
+            
+            NSLog(@"User logged out successfully");
+        } else {
+            NSLog(@"Error logging out: %@", error);
+        }
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
